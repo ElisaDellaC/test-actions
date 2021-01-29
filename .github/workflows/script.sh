aec53307cbcb194b968ff95f1bad6add4208966b
elisa-dc <41437937+elisa-dc@users.noreply.github.com>
2021-01-29 12:13:43 +0100

    skip on master

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -6,12 +6,19 @@ echo 'my trigger is: ' $TRIGGER_EVENT_NAME
             prNumber=$PR_NUM
           else
             sha=$WKFL_SHA
-            prNumber=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/search/issues\?q\="${sha}" | jq '. | .items' | jq '.[]' | jq '.number')
+            prNumber=$( curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/search/issues\?q\=8beefa0 | jq '. | .items' | jq '.[] | select(.state=="open")' | jq '.number')
           fi
 
-          echo my commit is: "${sha}"         
-          echo my PR is: "${prNumber}"
+          echo 'my commit is:' "${sha}"         
+          echo 'my PR is:' "${prNumber}"
           
+          numPrFound=$(echo $prNumber | jq -s '.' | jq length)
+
+          if [[ "$numPrFound" -eq 0 ]]; then
+            echo 'No pull request found for this commit. Has the wkfl been triggered by a push to master?' "${sha}" 
+            exit 0
+          fi
+
           numberSuccessCheckSuites=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/ElisaDellaC/test-actions/commits/"${sha}"/check-suites | jq '.check_suites' | jq '.[] | select(.app.slug=="github-actions") |.conclusion' | jq  'select(. == "success")' | jq -s '.' | jq length)
           numberPercyLabels=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/ElisaDellaC/test-actions/issues/"${prNumber}" | jq '.labels' | jq '.[] | .name' | jq 'select(. == "check-percy-results-before-merge")' | jq -s '.' | jq length)
 
